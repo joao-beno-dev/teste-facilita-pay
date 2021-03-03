bd8655ad9bd962843716215e8a37ad5a80d44342
João Beno Schreiner Junior <joao@beno.dev>
2021-03-03 12:05:26 -0300

    Seletor de moedas.

---
+++ src/components/SymbolPicker.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="row">
+    <div class="col-5 content-center">
+      <SymbolSelect :list="list" :selected="selected[0]" :disabled="selected[1]" v-on:set-moeda="setMoedaUm" />
+    </div>
+    <div class="col-2">
+      <q-btn round color="primary" icon="fas fa-exchange-alt" @click="atualizarMoedas" />
+    </div>
+    <div class="col-5">
+      <SymbolSelect :list="list" :selected="selected[1]" :disabled="selected[0]" v-on:set-moeda="setMoedaDois" />
+    </div>
+  </div>
+</template>
+
+<script>
+import SymbolSelect from "@/components/SymbolSelect";
+export default {
+  name: "SymbolPicker",
+  components: { SymbolSelect },
+  props: {
+    list: Array,
+    selected: Array
+  },
+  methods: {
+    atualizarMoedas: function() {
+      this.$emit("switch-coins");
+    },
+    setMoedaUm: function(val) {
+      if (val !== this.$props.selected[1]) {
+        this.$emit("set-first-currency", val);
+      }
+    },
+    setMoedaDois: function(val) {
+      if (val !== this.$props.selected[0]) {
+        this.$emit("set-second-currency", val);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/SymbolSelect.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-btn color="primary" :label="selected">
+    <q-menu>
+      <q-list style="min-width: 100px">
+        <q-item clickable v-close-popup v-for="symbol in list" :key="symbol" :class="symbol === selected ? 'hidden' : null">
+          <q-item-section
+            :disabled="symbol === disabled ? '' : null"
+            @click="trocarMoeda(symbol)"
+          >
+            {{ symbol }}
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-menu>
+  </q-btn>
+</template>
+
+<script>
+export default {
+  name: "SymbolSelect",
+  props: {
+    list: Array,
+    selected: String,
+    disabled: String
+  },
+  methods: {
+    trocarMoeda: function(val) {
+      this.$emit("set-moeda", val);
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
