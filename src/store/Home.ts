6ab009c0ec65b1f4659567132d5ddabc340a7997
João Beno Schreiner Junior <joao@beno.dev>
2021-03-03 12:06:43 -0300

    Ajustes na logica da página de conversão de moedas.

--- src/store/Home.ts
+++ src/store/Home.ts
@@ -7,15 +7,15 @@ Vue.use(Vuex);
 export const HomeStore = new Vuex.Store({
   state: {
     taxa: 0.0,
-    symbolFrom: "BRL",
-    symbolTo: "USD",
+    symbolFrom: "USD",
+    symbolTo: "BRL",
     valueBefore: 0.0,
     valueAfter: 0.0,
     alcanceGrafico: GraficoAlcance.TRIMESTRE,
     historico: []
   },
   mutations: {
-    increment(state, taxa) {
+    setTaxa(state, taxa) {
       state.taxa = taxa;
     },
     setSymbolFrom(state, value) {
@@ -25,16 +25,24 @@ export const HomeStore = new Vuex.Store({
       state.symbolTo = value;
     },
     setValueBefore(state, value) {
+      console.log(`4: ${value}`);
       state.valueBefore = value;
     },
     setValueAfter(state, value) {
       state.valueAfter = value;
     },
-    convertFromTo(state) {
-      state.valueAfter = state.valueBefore * state.taxa;
-    },
-    convertToFrom(state) {
-      state.valueBefore = state.valueAfter / state.taxa;
+    convert(state, invert = false) {
+      if (invert) {
+        console.log(`6b:\n\tantes: ${state.valueBefore}\n\tdepois: ${state.valueAfter}`);
+        state.valueBefore = state.valueAfter * state.taxa;
+        console.info(`--> Taxa: ${state.taxa}`);
+        console.log(`7b:\n\tantes: ${state.valueBefore}\n\tdepois: ${state.valueAfter}`);
+      } else {
+        console.log(`6a:\n\tantes: ${state.valueBefore}\n\tdepois: ${state.valueAfter}`);
+        state.valueAfter = state.valueBefore / state.taxa;
+        console.info(`--> Taxa: ${state.taxa}`);
+        console.log(`7a:\n\tantes: ${state.valueBefore}\n\tdepois: ${state.valueAfter}`);
+      }
     },
     setHistorico(state, historico) {
       state.historico = historico;
@@ -43,6 +51,15 @@ export const HomeStore = new Vuex.Store({
       state.alcanceGrafico = alcance;
     }
   },
-  actions: {},
+  actions: {
+    converterAB(store) {
+      console.log("5: converter A -> B");
+      store.commit("convert");
+    },
+    converterBA(store) {
+      console.log("5: converter A <- B");
+      store.commit("convert", true);
+    }
+  },
   modules: {}
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,10 +6,20 @@
 
 <template>
   <div class="home container">
-    <Calculadora :taxa="taxa" class="row flex-center" />
+    <SymbolPicker :list="['BRL', 'USD', 'EUR']" :selected="[simboloUm, simboloDois]" v-on:switch-coins="switchCoins"
+      v-on:set-first-currency="setFirstCurrency" v-on:set-second-currency="setSecondCurrency"/>
+    <Calculadora
+      :valor-converter="valorUm"
+      :valor-convertido="valorDois"
+      v-on:antes="atualizarAntes"
+      :simbolo-um="simboloUm"
+      :simbolo-dois="simboloDois"
+      v-on:depois="atualizarDepois"
+      class="row flex-center"
+    />
     <Grafico :historico="historico" :alcance="alcance" class="grafico row" />
     <div class="row flex-center">
-    <span class="q-mr-lg text-accent">Selecione o intervalo:</span>
+      <span class="q-mr-lg text-accent">Selecione o intervalo:</span>
       <q-btn-group outline>
         <q-btn :outline="alcance === 0 ? null : true" color="accent" label="Ano" @click="mudarAlcance(0)" />
         <q-btn :outline="alcance === 1 ? null : true" color="accent" label="Semestre" @click="mudarAlcance(1)" />
@@ -27,9 +37,16 @@
 import { Component, Vue } from "vue-property-decorator";
 import { HomeStore } from "@/store";
 import Calculadora from "@/components/Calculadora.vue";
-import { TaxaEm } from "@/model";
-import Grafico from "@/components/Grafico.vue"; // @ is an alias to /src
+import { Taxa, TaxaEm } from "@/model";
+import Grafico from "@/components/Grafico.vue";
+import SymbolPicker from "@/components/SymbolPicker.vue"; // @ is an alias to /src
 
+/**
+ * Obtem o dia aproximado do ano de uma determinada Data.
+ * Usado apenas para ordenar o histórico de cotações
+ * @param {Date} data - A data a contar os dias.
+ * @returns {number} a posição aproximada da data no ano.
+ */
 function diaDoAno(data: Date): number {
   const start = new Date(data.getFullYear(), 0, 0);
   const diff = data - start;
@@ -37,7 +54,12 @@ function diaDoAno(data: Date): number {
   return Math.floor(diff / oneDay);
 }
 
-function sortHistorico(taxas: TaxaEm): Array {
+/**
+ * Ordena a resposta da API de acordo com a data das Cotações.
+ * @param {TaxaEm} taxas - Dicionário com as datas e as cotações.
+ * @returns {Array} Retorna uma array com as cotações.
+ */
+function sortHistorico(taxas: TaxaEm): Array<{ data: Date; rate: Taxa }> {
   const hist = Array(366);
 
   for (const dt in taxas) {
@@ -56,6 +78,7 @@ function sortHistorico(taxas: TaxaEm): Array {
 @Component({
   store: HomeStore,
   components: {
+    SymbolPicker,
     Grafico,
     Calculadora
   },
@@ -68,24 +91,90 @@ function sortHistorico(taxas: TaxaEm): Array {
     },
     alcance() {
       return this.$store.state.alcanceGrafico;
+    },
+    valorUm() {
+      return this.$store.state.valueBefore;
+    },
+    valorDois() {
+      return this.$store.state.valueAfter;
+    },
+    simboloUm() {
+      return this.$store.state.symbolFrom;
+    },
+    simboloDois() {
+      return this.$store.state.symbolTo;
     }
   },
-  beforeMount: function() {
-    this.$api.forex.getTaxa("BRL", "USD").then(resposta => {
-      this.$store.commit("increment", resposta.data.rates["BRL"]);
-    });
+  mounted: function() {
 
-    const today = new Date();
-    const yesteryear = new Date((new Date()).setFullYear(today.getFullYear() - 1));
+    this.refreshQuotes();
 
-    this.$api.forex.getHistorico("USD", "BRL", yesteryear, today)
-      .then(resposta =>
-        this.$store.commit("setHistorico", sortHistorico(resposta.data.rates))
-      );
+    this.refreshHistory();
+
   },
   methods: {
-    mudarAlcance: function (alcance) {
+    mudarAlcance: function(alcance) {
       this.$store.commit("setAlcance", alcance);
+    },
+    atualizarAntes: function(val) {
+      console.log(`2a: ${val}`);
+      const numVal = (typeof val === "number") ? val : Number.parseFloat(val.replace(/[.,](?=\d{3})|(^.{1,3}\s)/g, "").replace(/[,](?=\d{2}$)/g, "."));
+      console.log(`3a: ${numVal}`);
+      this.$store.commit("setValueBefore", numVal);
+      this.$store.dispatch("converterAB");
+    },
+    atualizarDepois: function(val) {
+      console.log(`2b: ${val}`);
+      const numVal = (typeof val === "number") ? val : Number.parseFloat(val.replace(/[.,](?=\d{3})|(^.{1,3}\s)/g, "").replace(/[,](?=\d{2}$)/g, "."));
+      console.log(`3b: ${numVal}`);
+      this.$store.commit("setValueAfter", numVal);
+      this.$store.dispatch("converterBA");
+    },
+    switchCoins: function() {
+      const tempSymbol = this.$store.state.symbolFrom;
+      const tempValue = this.$store.state.valueBefore;
+
+      this.$store.commit("setValueBefore", this.$store.state.valueAfter);
+      this.$store.commit("setValueAfter", tempValue);
+
+      this.$store.commit("setSymbolFrom", this.$store.state.symbolTo);
+      this.$store.commit("setSymbolTo", tempSymbol);
+
+      Promise.allSettled([this.refreshQuotes(), this.refreshHistory()]).then(() => {
+        this.$store.dispatch("converterAB");
+      });
+    },
+    refreshQuotes: function(): Promise<boolean> {
+      return new Promise<boolean>((resolve, reject) =>
+          this.$api.forex.getTaxa(this.$store.state.symbolFrom, this.$store.state.symbolTo)
+        .then(resposta => {
+          this.$store.commit("setTaxa", resposta.data.rates[this.$store.state.symbolFrom]);
+        }).finally(() => resolve(true))
+      );
+    },
+    refreshHistory: function(): Promise<boolean> {
+      return new Promise<boolean>((resolve, reject) => {
+        const today = new Date();
+        const yesteryear = new Date((new Date()).setFullYear(today.getFullYear() - 1));
+
+        this.$api.forex.getHistorico(this.$store.state.symbolTo, this.$store.state.symbolFrom, yesteryear, today)
+            .then(resposta =>
+                this.$store.commit("setHistorico", sortHistorico(resposta.data.rates))
+            )
+            .finally(() => resolve(true));
+      });
+    },
+    setFirstCurrency: function(val) {
+      this.$store.commit("setSymbolFrom", val);
+      Promise.allSettled([this.refreshQuotes(), this.refreshHistory()]).then(() => {
+        this.$store.dispatch("converterAB");
+      });
+    },
+    setSecondCurrency: function(val) {
+      this.$store.commit("setSymbolTo", val);
+      Promise.allSettled([this.refreshQuotes(), this.refreshHistory()]).then(() => {
+        this.$store.dispatch("converterAB");
+      });
     }
   }
 })
